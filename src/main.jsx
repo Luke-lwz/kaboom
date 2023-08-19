c1bc2bdd519716cf5bc8461b312514d3c2f2354f
Luke-lwz <77695529+Luke-lwz@users.noreply.github.com>
2023-08-19 14:28:03 +0000

    skrrraaaa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { render } from 'preact';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App'
 import './index.css'
 
 render(
   <BrowserRouter>
+    <Analytics mode={'production'} />;
     <App />
   </BrowserRouter>,
   document.getElementById('root')
--- src/views/Privacy.jsx
+++ src/views/Privacy.jsx
@@ -13,6 +13,13 @@ function Privacy(props) {
 
             <p>Kaboom follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
 
+            <ul>
+                <li className='ml-4'>
+                    <p>Vercel Analytics</p>
+                    <p><a className="text-blue-500 underline" href="https://vercel.com/docs/analytics/privacy-policy">Privacy Policy</a></p>
+                </li>
+            </ul>
+
             <h2 className='text-lg font-bold'>Cookies and Web Beacons</h2>
 
             <p>Like any other website, Kaboom uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
