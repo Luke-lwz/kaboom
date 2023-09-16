7fd8679f79f3127b2e33c2069490d2c96fc78440
Luke-lwz <lukaslunkwitz@gmail.com>
2023-09-16 18:57:55 +0200

    added playsets

--- src/views/LobbyView.jsx
+++ src/views/LobbyView.jsx
@@ -267,6 +267,8 @@ function HostLobby({ me, code }) {
         setWrongPlayerNumber(calculatePlaysetDisabled(playset, players.current.length));
 
 
+
+
         setRecommendBury(playset?.odd_card ? false : ((playset?.cards.filter(c => c?.id !== "p001")?.length + (playset?.odd_card ? 1 : 0)) % 2) !== (players.current.length % 2)); // filters out drunk card
 
 
@@ -565,7 +567,7 @@ function HostLobby({ me, code }) {
                 <h1 className='font-extrabold text-lg uppercase flex items-center gap-2'>Selected Playset <Info tooltip="Playsets are predetermined decks of cards, that will be distributed among players. They often change the feel of the entire game, so choose wisely." /></h1>
                 {wrongPlayerNumber && <WrongPlayerNumberPlayset />}
                 <PlaysetDisplay forceOpen selected onClick={() => showAllPlaysets()} playset={playset} />
-                <PlayWithBuryToggle recommendBury={recommendBury} bury={(playWithBury) && !playset?.no_bury} onChange={bury => setPlayWithBury(bury)} disabled={!playset?.odd_card || playset?.no_bury || playset?.force_bury} />
+                <PlayWithBuryToggle recommendBury={recommendBury} bury={(playWithBury) && !playset?.no_bury} onChange={bury => setPlayWithBury(bury)} disabled={ playset?.no_bury || playset?.force_bury} />
             </div>
 
             <LobbyFooter />
