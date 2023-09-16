2889970efc43561d8cc172202351707566da24ce
Luke-lwz <lukaslunkwitz@gmail.com>
2023-09-16 16:44:57 +0200

    swag

--- src/components/menus/PlayerSelectMenu.jsx
+++ src/components/menus/PlayerSelectMenu.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react"
 
 import SelectGrid, { SelectGridBox } from "../SelectGrid";
 
+import Avatar, { genConfig } from 'react-nice-avatar-vite-prod-fork'
 
-function PlayerSelectMenu({ players, onSelect = () => {}, titleElement, preSelectedPlayers = [], color, buttonText }) {
+
+
+function PlayerSelectMenu({ players, onSelect = () => { }, titleElement, preSelectedPlayers = [], color, buttonText }) {
 
     const [selectedArray, setSelectedArray] = useState([...preSelectedPlayers])
 
@@ -26,14 +29,23 @@ function PlayerSelectMenu({ players, onSelect = () => {}, titleElement, preSelec
                 {titleElement}
             </div>
             <SelectGrid className={"h-fit max-h-[16rem] overflow-y-scroll overflow-x-hidden scrollbar-hide"}>
-                {players.map(player => <SelectGridBox color={color} key={player?.id} onClick={() => handleSelect(player?.id)} selected={(selectedArray?.includes(player?.id))}>
-                    {player?.name}
-                </SelectGridBox>)}
-                
-                
+                {players.map(player => {
+                    const avaConfig = genConfig(player?.name || player?.id || "Josh")
+                    return (<SelectGridBox color={color} key={player?.id} onClick={() => handleSelect(player?.id)} selected={(selectedArray?.includes(player?.id))}>
+                        <div className="px-4 py-6 flex flex-col items-center w-full justify-center">
+                            <div className=" h-20 w-20">
+                                <Avatar className="w-full h-full" {...avaConfig} />
+                            </div>
+                            <h3 className="truncate text-title font-extrabold w-full pt-2">{player?.name}wefwefwefwefwef</h3>
+                        </div>
+
+                    </SelectGridBox>)
+                })}
+
+
             </SelectGrid>
             <div className="w-full flex items-center flex-row-reverse justify-start">
-                <button onClick={() => onSelect(selectedArray)} style={{backgroundColor: color, borderColor: color}} className={" btn noskew w-full mt-3 transition-all text-white " + (selectedArray?.length <=0 ? " opacity-50 " : " opacity-100 clickable ")}>{buttonText || "SELECT"} </button>
+                <button onClick={() => onSelect(selectedArray)} style={{ backgroundColor: color, borderColor: color }} className={" btn noskew w-full mt-3 transition-all text-white " + (selectedArray?.length <= 0 ? " opacity-50 " : " opacity-100 clickable ")}>{buttonText || "SELECT"} </button>
 
             </div>
         </div>
