b42ab6204db2ecec6477a6d8f1f2a74353498018
Luke-lwz <lukaslunkwitz@gmail.com>
2024-01-27 21:18:17 +0100

    wefwef

--- src/helpers/peerid.js
+++ src/helpers/peerid.js
@@ -13,16 +13,18 @@ export function getPeerConfig() {
     secure: true,
     host: "peerjs-kaboom-custom-server.onrender.com",
     port: 443,
-    config: {
-      iceServers: [
-        { url: "stun:stun.l.google.com:19302" },
-        {
-          url: "turn:matze-server.net:3478",
-          credential:
-            "5c4750c474320b5fdc63470be5d6a4a3c4b4a89ed67894b345b4e3515196d12d",
-        },
-      ],
-    } /* Sample servers, please use appropriate ones */,
+    key: "peerjs",
+    allow_discovery: true,
+    // config: {
+    //   iceServers: [
+    //     // { url: "stun:matze-server.net:3478" },
+    //     {
+    //       url: "turn:matze-server.net:3478",
+    //       credential:
+    //         "5c4750c474320b5fdc63470be5d6a4a3c4b4a89ed67894b345b4e3515196d12d",
+    //     },
+    //   ],
+    // },
   };
 
   return undefined;
