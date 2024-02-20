bf63025372d6ec3caa731afa272ed13dc4fe6e08
Luke-lwz <lukaslunkwitz@gmail.com>
2024-02-20 12:58:08 +0100

    made deleting possible

--- src/views/playsets/PlaysetView.jsx
+++ src/views/playsets/PlaysetView.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useContext, useMemo, useState, useEffect } from "react"
+import { useContext, useMemo, useState, useEffect, useCallback } from "react"
 import { PageContext } from "../../components/PageContextProvider";
 import { getPlaysetById, maximizePlayset } from "../../helpers/playsets";
 import { PlaysetSimulator, TitleBar } from "./WorkbenchView";
@@ -9,12 +9,15 @@ import PlaysetDisplay from "../../components/playsets/PlaysetDisplay";
 //icons
 import { BsCassetteFill } from "react-icons/bs"
 import { BigAbsoluteMakeButton, BookmarkMegaButton, DeletePlaysetButton, EditPlaysetButton, RemixButton } from "../../components/MegaButtons";
+import supabase from "../../supabase";
+import toast from "react-hot-toast";
+import { promiser } from "../../helpers/promiser";
 
 function PlaysetView({ }) {
 
     const { id } = useParams();
 
-    const { smoothNavigate, user } = useContext(PageContext)
+    const { smoothNavigate, user, setPrompt, checkAuthenticated } = useContext(PageContext)
 
     if (!id) smoothNavigate("/playsets")
 
@@ -41,6 +44,35 @@ function PlaysetView({ }) {
     }
 
 
+    const deletePlayset = useCallback(() => {
+        setPrompt({
+            title: "Delete Playset?",
+            text: "This cannot be undone.",
+            onApprove: async () => {
+
+                const { promise, promiseResolve, promiseReject } = promiser();
+
+                toast.promise(promise, {
+                    loading: 'Deleting Playset',
+                    success: 'Playset deleted',
+                    error: 'Error while deleting playset',
+                })
+
+                const { error } = await supabase
+                    .from("playsets")
+                    .delete()
+                    .eq("id", id)
+                if (error) {
+                    promiseReject(error);
+                } else {
+                    promiseResolve();
+                    smoothNavigate("/playsets");
+                }
+            }
+        })
+    }, [id])
+
+
     return (
         <div className="flex flex-col lg:flex-row w-full h-full overflow-x-hidden overflow-y-scroll scrollbar-hide pb-64">
 
@@ -59,10 +91,12 @@ function PlaysetView({ }) {
                 <div className="w-full max-w-2xl p-4 flex flex-col items-center">
                     {playsetMaximized && <PlaysetDisplay quickActions={{ vote: true, profile: true }} forceOpen playset={playsetMaximized} />}
                     <div className="w-full grid grid-cols-2 gap-2 mt-2">
-                        <RemixButton onClick={() => smoothNavigate(`/workbench/${playset?.id}/remix`)} />
+                        <RemixButton onClick={() => checkAuthenticated(() => smoothNavigate(`/workbench/${playset?.id}/remix`))} />
                         <BookmarkMegaButton />
-                        <EditPlaysetButton onClick={() => smoothNavigate(`/workbench/${playset?.id}/edit`)} />
-                        <DeletePlaysetButton />
+                        {playset?.user_id === user?.id && <>
+                            <EditPlaysetButton onClick={() => checkAuthenticated(() => smoothNavigate(`/workbench/${playset?.id}/edit`))} />
+                            <DeletePlaysetButton onClick={() => checkAuthenticated(() => deletePlayset())} />
+                        </>}
                     </div>
                     {playset?.description && <div className=" text-base-content py-4 w-full mt-2">
                         {playset?.description}
